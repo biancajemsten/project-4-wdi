10543e2d8856a8e71b948525c13f03b37308ab22
Bianca Jemsten <biancajemsten@gmail.com>
2018-07-10 16:00:15 +0100

    changed date format and created eventDate virtual

--- models/event.js
+++ models/event.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const moment = require('moment');
 
 
 const timeSlotSchema = new mongoose.Schema({
@@ -22,5 +23,30 @@ const eventSchema = new mongoose.Schema({
   image: String,
   organizer: { type: mongoose.Schema.ObjectId, ref: 'User' }
 });
+
+// eventSchema.pre('save', function(next){
+//   this.timeSlots.forEach(timeSlot=>{
+//     const date = timeSlot.date;
+//     timeSlot.date = moment(date).format('YYYY-MM-DD');
+//     console.log(timeSlot.date);
+//     return timeSlot.date;
+//   });
+//   next();
+// });
+
+
+timeSlotSchema.path('date')
+  .get(function formatDate(date){
+    return moment(date).format('ddd, MMM Do');
+  });
+
+
+eventSchema.virtual('eventDates')
+  .get(function() {
+    return Array.from(new Set(this.timeSlots.map(slot => slot.date)));
+  });
+
+eventSchema.set('toJSON', { virtuals: true });
+
 
 module.exports = mongoose.model('Event', eventSchema);
--- src/components/events/Show.js
+++ src/components/events/Show.js
@@ -13,21 +13,40 @@ class EventsShow extends React.Component{
   componentDidMount(){
     axios.get(`/api/events/${this.props.match.params.id}`)
       .then(res => this.setState({event: res.data}))
+      .then(() => console.log(this.state))
       .catch(err => this.setState({error: err.message}));
   }
+
 
   render(){
     if(!this.state.event) return <h2 className="title">Loading...</h2>;
     return(
-      <div className="columns is-multiline">
-        <div className="column is-full">
-          <figure className="image">
-            <img src={this.state.event.image}/>
-          </figure>
+      <div>
+        <h2 className="title is-2">{this.state.event.name}</h2>
+        <div className="columns is-multiline is-mobile">
+          <div className="column is-one-third-mobile">
+            <figure className="image is-128x128">
+              <img src={this.state.event.image}/>
+            </figure>
+          </div>
+          <div className="column is-two-thirds-mobile">
+            <p><strong>Address: </strong>{this.state.event.address}</p>
+            <p><strong>Description: </strong>{this.state.event.description}</p>
+            {this.state.event.finalTime && <p><strong>Event Time: </strong>{this.state.event.finalTime}</p>}
+          </div>
         </div>
+        <div className="columns is-full is-mobile">
+          {this.state.event.eventDates.map(date =>
+            <div key={date} className="column is-one-third-mobile">
+              <div>{date}</div>
+            </div>
+          )}
+        </div>
+
+
       </div>
     );
   }
 }
 
-export default EventsShow; 
+export default EventsShow;
