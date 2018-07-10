f38981cf5a5bc567905f8788a3a8c732040b6ee1
Richard Tzanov <richardtzanov@gmail.com>
2018-07-10 13:44:15 +0100

    created index filter

--- src/components/events/Index.js
+++ src/components/events/Index.js
@@ -15,10 +15,24 @@ class EventsIndex extends React.Component {
       });
   }
 
+  handleSearch = (e) => {
+    this.setState({ search: e.target.value });
+  }
+
+  filteredEvents = (events) => {
+    const re = new RegExp(this.state.search, 'i');
+    return events.filter(event => {
+      return re.test(event.name);
+    });
+  }
+
   render() {
     if(!this.state.events) return <h2 className="title is-2">Loading...</h2>;
     return(
       <section>
+        <div className="filters">
+          <input className="input" placeholder="Search events" onChange={this.handleSearch} />
+        </div>
         <table className="table is-striped is-bordered is-hoverable is-fullwidth">
 
           <thead>
@@ -36,16 +50,17 @@ class EventsIndex extends React.Component {
           </thead>
 
           <tbody>
-            {this.state.events.map(event =>
+            {this.filteredEvents(this.state.events).map(event =>
               <tr key={event._id}>
                 <td>{event.name}</td>
                 <td>{event.address}</td>
-                <td>{event.organizer[0].username}</td>
+                <td>{event.organizer.username}</td>
               </tr>
             )}
           </tbody>
 
         </table>
+
       </section>
     );
   }
