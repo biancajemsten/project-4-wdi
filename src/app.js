d7fcd57834de597e82b568ef628e6b0960339c5d
MartinKoeoep <herrkoop@gmail.com>
2018-07-10 14:56:30 +0100

    added a not found page

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 import Navbar from './components/common/Navbar';
 import EventsIndex from './components/events/Index';
 import AuthRegister from './components/auth/Register';
@@ -23,6 +24,7 @@ class App extends React.Component {
                 <Route path="/events" component={EventsIndex} />
                 <Route path="/register" component={AuthRegister} />
                 <Route path="/login" component={AuthLogin} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </section>
