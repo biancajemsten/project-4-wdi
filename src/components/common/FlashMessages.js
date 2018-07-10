6a9a91d3b56ff5bdcab1687be04c4ac9b0901bf4
Richard Tzanov <richardtzanov@gmail.com>
2018-07-10 14:56:32 +0100

    created flashMessages

---
+++ src/components/common/FlashMessages.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Flash from '../../lib/Flash';
+
+const FlashMessages = () => {
+  const messages = Flash.getMessages();
+  Flash.clearMessages();
+
+  return(
+    <div className="container">
+      {messages && Object.keys(messages).map(type =>
+        <div key={type} className={`notification is-${type}`}>{messages[type]}</div>
+      )}
+    </div>
+  );
+};
+
+export default FlashMessages;
