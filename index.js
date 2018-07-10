6bd4716db6690721234fc5e807900573ec18ee2c
Bianca Jemsten <biancajemsten@gmail.com>
2018-07-10 09:57:23 +0100

    added errorHandler

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 4000;
+const errorHandler = require('./lib/errorHandler');
+
 
 app.use(express.static(`${__dirname}/public`));
 
 
 
 app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Express running on port ${port}`));
 
