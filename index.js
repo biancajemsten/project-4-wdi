6a6e7be5d7c92d54dff7a7f117b13916d0060f1e
Richard Tzanov <richardtzanov@gmail.com>
2018-07-10 11:17:20 +0100

    clarified names in index.js

--- index.js
+++ index.js
@@ -3,23 +3,17 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const errorHandler = require('./lib/errorHandler');
-const routes = require('./config/routes');
-const { dbURI, port } = require('./config/environment');
-const mongoose = require('mongoose');
-
 
 const router = require('./config/routes');
 
+const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
+const { dbURI, port } = require('./config/environment');
 mongoose.connect(dbURI);
-
-app.use(bodyParser.json());
-app.use('/api', routes);
-app.use(express.static(`${__dirname}/public`));
-
-
-app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+// 
+// app.use(express.static(`${__dirname}/public`));
+// app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
 app.use(bodyParser.json());
 app.use('/api', router);
