458f7e553a9545ebc66328cb8f11d0b8c5d949f8
Bianca Jemsten <biancajemsten@gmail.com>
2018-07-10 10:09:38 +0100

    added secureRoute

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const events = require('../controllers/events');
+const secureRoute = require('../lib/secureRoute'); 
 
 router.route('/events')
   .get(events.index);
