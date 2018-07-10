7d889114b057358217870b163a805f313ce9f483
Richard Tzanov <richardtzanov@gmail.com>
2018-07-10 09:52:16 +0100

    created index controller and route

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+const router = require('./config/router');
+
 app.use(express.static(`${__dirname}/public`));
 
 
 
 app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
+app.use('/api', router);
 
 app.listen(port, () => console.log(`Express running on port ${port}`));
 
