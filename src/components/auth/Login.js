6a9507ae3fab09a14e37478b8f4c0bb9a245da9e
Bianca Jemsten <biancajemsten@gmail.com>
2018-07-10 12:12:26 +0100

    added register component

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import axios from 'axios';
+import Auth from '../lib/Auth';
+import Flash from '../lib/Flash';
+
+class AuthLogin extends React.Component{
+  state={}
+
+  handleSubmit = (e) =>{
+    e.preventDefault();
+    axios({
+      method: 'POST',
+      data: this.state
+    });
+  }
+}
+export default AuthLogin; 
---
+++ src/components/auth/Register.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import axios from 'axios';
+import Auth from '../../lib/Auth';
+
+class AuthRegister extends React.Component{
+  state= {}
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    axios({
+      url: '/api/register',
+      method: 'POST',
+      data: this.state
+    })
+      .then(res => {
+        Auth.setToken(res.data.token);
+        this.props.history.push('/login');
+      });
+  }
+
+  handleChange = ({target: {name, value}}) => {
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <div className="field">
+          <label className="username">Username</label>
+          <input className="input" name="username" placeholder="Username" onChange={this.handleChange}/>
+        </div>
+        <div className="field">
+          <label className="email">Email</label>
+          <input className="input" name="email" placeholder="Email" onChange={this.handleChange}/>
+        </div>
+        <div className="field">
+          <label className="password">Password</label>
+          <input className="input" type="password" name="password" placeholder="Password" onChange={this.handleChange}/>
+        </div>
+        <div className="field">
+          <label className="passwordConfirmation">Password Confirmation</label>
+          <input className="input" type="password" name="passwordConfirmation" placeholder="Password Confirmation" onChange={this.handleChange}/>
+        </div>
+
+        <button className="button">Submit</button>
+      </form>
+    );
+  }
+
+}
+
+export default AuthRegister;
