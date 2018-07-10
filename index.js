5b33d757a3e3dc1fcfb54fa9aa38fe201c39eb88
MartinKoeoep <herrkoop@gmail.com>
2018-07-10 10:09:47 +0100

    Added dependancies, authentication for register and login

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
-const port = 4000;
+const bodyParser = require('body-parser');
+
+const routes = require('./config/routes');
+const { dbURI, port }= require('./config/environment');
+const mongoose = require('mongoose');
 
 app.use(express.static(`${__dirname}/public`));
 
+mongoose.connect(dbURI);
 
+app.use(bodyParser.json());
+app.use('/api', routes);
 
 app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
