0385d050420b08727773b9074840098d37e1a0e8
Bianca Jemsten <biancajemsten@gmail.com>
2018-07-12 22:15:55 +0100

    styled table on index page

--- src/components/events/Index.js
+++ src/components/events/Index.js
@@ -57,47 +57,22 @@ class EventsIndex extends React.Component {
         </div>
 
         <div className="columns is-multiline is-mobile indexTable">
-          <div className="column is-full-mobile is-full-desktop indexHeader">
+          <div className="column is-full-mobile is-full-desktop is-full-tablet indexHeader">
             <h6 className="title is-6">Event</h6>
             <h6 className="title is-6">Location</h6>
             <h6 className="title is-6">Organizer</h6>
           </div>
-          <div className="is-full-mobile is-full-desktop">
+          <div className="column columns is-full-mobile is-full-desktop is-full-tablet indexList is-multiline">
             {this.sortedAndFilteredEvents(this.state.events).map(event =>
-              <Link to={`/events/${event._id}`} key={event._id}><div>
-                <td>{event.name}</td>
-                <td>{event.address}</td>
-                <td>{event.organizer.username}</td>
-              </div></Link>
+              <div className="column columns is-full-mobile is-full-desktop is-full-tablet" key={event._id}><Link to={`/events/${event._id}`}>
+                <div className="column is-one-third-mobile is-one-third-desktop rightItem"><p>{event.name}</p></div>
+                <div className="column is-one-third-mobile is-one-third-desktop"><p>{event.address}</p></div>
+                <div className="column is-one-third-mobile is-one-third-desktop leftItem"><p>{event.organizer.username}</p></div>
+              </Link></div>
             )}
           </div>
         </div>
 
-        <table className="table is-striped is-bordered is-hoverable is-fullwidth">
-          <thead>
-            <tr>
-              <th>
-                Event
-              </th>
-              <th>
-                Location
-              </th>
-              <th>
-                Organizer
-              </th>
-            </tr>
-          </thead>
-
-          <tbody className="">
-            {this.sortedAndFilteredEvents(this.state.events).map(event =>
-              <Link to={`/events/${event._id}`} key={event._id}><tr>
-                <td>{event.name}</td>
-                <td>{event.address}</td>
-                <td>{event.organizer.username}</td>
-              </tr></Link>
-            )}
-          </tbody>
-        </table>
         <p className ="font-is-light"><strong>Lorem ipsum dolor sit amet</strong>, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
       </section>
     );
