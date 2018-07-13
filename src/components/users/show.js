9baa832031c863cc8231a5c426e141c61da8f4b3
Richard Tzanov <richardtzanov@gmail.com>
2018-07-13 11:37:57 +0100

    added user show to front end

---
+++ src/components/users/show.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class UsersShow extends React.Component {
+  render() {
+    return(
+      <h1 className="title is-1">USERS</h1>
+    );
+  }
+}
+
+export default UsersShow;
