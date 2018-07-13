8fc1d0db20e78e834765ecc07fe9c75adab938e8
Richard Tzanov <richardtzanov@gmail.com>
2018-07-13 11:33:09 +0100

    added users show and update controllers and routes

--- controllers/users.js
+++ controllers/users.js
@@ -7,4 +7,24 @@ function indexRoute(req, res, next) {
     .catch(next);
 }
 
-module.exports = { index: indexRoute };
+function showRoute(req, res, next) {
+  User
+    .findById(req.params.id)
+    .then(user => res.json(user))
+    .catch(next);
+}
+
+function updateRoute(req, res, next) {
+  User
+    .findById(req.params.id)
+    .then(user => user.set(req.body))
+    .then(user => user.save())
+    .then(user => res.json(user))
+    .catch(next);
+}
+
+module.exports = {
+  index: indexRoute,
+  show: showRoute,
+  update: updateRoute
+};
