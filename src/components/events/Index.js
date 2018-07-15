844f272061d53c2b1839e1a0c4e5dc3815c8f3f0
Bianca Jemsten <biancajemsten@gmail.com>
2018-07-15 10:49:35 +0100

    index style

--- src/components/events/Index.js
+++ src/components/events/Index.js
@@ -69,9 +69,9 @@ class EventsIndex extends React.Component {
           <div className="column columns is-full-mobile is-full-desktop is-full-tablet indexList is-multiline">
             {this.sortedAndFilteredEvents(this.state.events).map(event =>
               <div className="column columns is-full-mobile is-full-desktop is-full-tablet" key={event._id}><Link to={`/events/${event._id}`}>
-                <div className="column is-one-third-mobile is-one-third-desktop rightItem"><p>{event.name}</p></div>
-                <div className="column is-one-third-mobile is-one-third-desktop"><p>{event.address}</p></div>
-                <div className="column is-one-third-mobile is-one-third-desktop leftItem"><p>{event.organizer.username}</p></div>
+                <div className="column is-one-third-mobile is-one-third-desktop"><p>{event.name}</p></div>
+                <div className="column is-one-third-mobile is-one-third-desktop middleItem"><p>{event.address}</p></div>
+                <div className="column is-one-third-mobile is-one-third-desktop"><p>{event.organizer.username}</p></div>
               </Link></div>
             )}
           </div>
