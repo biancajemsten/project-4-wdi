8875f9974141e8ca6c4e23321d0be2016d98f36a
Richard Tzanov <richardtzanov@gmail.com>
2018-07-15 11:03:01 +0100

    submit votes button does not appear if organizer picks dates without voting

--- src/components/events/Show.js
+++ src/components/events/Show.js
@@ -212,7 +212,7 @@ class EventsShow extends React.Component{
         </div>}
         <div className="buttonDiv">
           {!this.state.event.finalTimesChecker && this.state.finalTimes.length > 0 && <button className="button" onClick={this.handleSubmit}>Confirm Times</button>}
-          {!this.checkUserAttending() && <button className="button" onClick={this.handleVoteSubmit}>Submit Votes</button>}
+          {!this.checkUserAttending() && !this.state.event.finalTimesChecker && <button className="button" onClick={this.handleVoteSubmit}>Submit Votes</button>}
         </div>
         {this.state.event.finalTimesChecker && <h2 className="title is-2">Selected Times</h2>}
         {this.state.event.finalTimesChecker && <div className="columns is-full is-mobile is-multiline">
