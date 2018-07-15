ba00913eab50b2895e4efc702fc6b7dd2ace9138
Bianca Jemsten <biancajemsten@gmail.com>
2018-07-15 10:43:52 +0100

    added final times twilio message

--- controllers/events.js
+++ controllers/events.js
@@ -1,5 +1,6 @@
 const Event = require('../models/event');
 const { sendSMS } = require('../lib/twilio');
+const moment = require('moment');
 
 function indexRoute(req, res, next) {
   Event
@@ -13,6 +14,7 @@ function showRoute(req, res, next) {
   Event
     .findById(req.params.id)
     .populate('invitees')
+    .populate('organizer')
     .then(event => res.json(event))
     .catch(next);
 }
@@ -39,7 +41,21 @@ function updateRoute(req, res, next) {
     .findById(req.params.id)
     .then(event => event.set(req.body))
     .then(event => event.save())
-    .then(event => res.json(event))
+    .then(event => {
+      res.json(event);
+      if(req.body.finalTimes.length >0 && req.body.invitees.length > 0){
+        req.body.invitees.forEach(person =>{
+          const formattedTimes = req.body.finalTimes.map(time => {
+            return moment(time).format('dddd MMMM Do [at] HH:mm');
+          });
+          const times = formattedTimes.map(x => x).join(', ').replace(/(.*),(.*)$/, '$1 &$2');
+          const body = `Hi ${person.username}! ${req.body.organizer.username} has set the final time(s) for the event ${req.body.name}. It will take place on ${times}. For more information, visit http://localhost:8000/events/${event._id}`;
+          const tel = person.tel;
+          sendSMS(body, tel);
+        });
+      }
+      console.log(req.body);
+    })
     .catch(next);
 }
 
