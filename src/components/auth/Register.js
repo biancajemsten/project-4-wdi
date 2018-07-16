7fa9d15e1993c8e37f6f13cc93ce5dfc6d79131a
Richard Tzanov <richardtzanov@gmail.com>
2018-07-16 11:53:30 +0100

    form checks for valid uk phone number

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -35,6 +35,9 @@ class AuthRegister extends React.Component{
     } else if(name === 'email') {
       const re = new RegExp(/.+@.+\..+/);
       errorMessage = re.test(value) ? '' : 'Please enter a valid email address';
+    } else if(name === 'tel') {
+      const re = new RegExp(/^\(?(?:(?:0(?:0|11)\)?[\s-]?\(?|\+)44\)?[\s-]?\(?(?:0\)?[\s-]?\(?)?|0)(?:\d{2}\)?[\s-]?\d{4}[\s-]?\d{4}|\d{3}\)?[\s-]?\d{3}[\s-]?\d{3,4}|\d{4}\)?[\s-]?(?:\d{5}|\d{3}[\s-]?\d{3})|\d{5}\)?[\s-]?\d{4,5}|8(?:00[\s-]?11[\s-]?11|45[\s-]?46[\s-]?4\d))(?:(?:[\s-]?(?:x|ext\.?\s?|#)\d+)?)$/);
+      errorMessage = re.test(value) ? '' : 'Please enter a valid UK telephone number';
     } else {
       errorMessage = value.length === 0 ? 'This field is required' : '';
     }
@@ -61,7 +64,7 @@ class AuthRegister extends React.Component{
         </div>
         <div className="field">
           <label className="tel">Telephone Number</label>
-          <input className="input" name="tel" placeholder="Telephone Number" onChange={this.handleChange} onBlur={this.handleBlur} value="+44" />
+          <input className="input" name="tel" placeholder="Telephone Number" onChange={this.handleChange} onBlur={this.handleBlur} />
           {this.state.errors.tel && <small>{this.state.errors.tel}</small>}
         </div>
         <div className="field">
