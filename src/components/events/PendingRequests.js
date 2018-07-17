869a56ef91380cf949924e1e2ac4c863b1eed410
Bianca Jemsten <biancajemsten@gmail.com>
2018-07-17 17:03:59 +0100

    button styles

--- src/components/events/PendingRequests.js
+++ src/components/events/PendingRequests.js
@@ -6,7 +6,7 @@ const PendingRequests = ({ event, acceptRequest, declineRequest }) => {
       {event.joinRequests.length > 0 && <h3 className="title is-3">Pending Requests</h3>}
       <div className="requestTable columns is-multiline is-mobile">
         {event.joinRequests.map(request =>
-          <div className="column is-full" key={request._id}>
+          <div className="column is-two-fifths-desktop is-three-fifths-mobile" key={request._id}>
             <p>{request.username}</p>
             <div className="requestButtonContainer">
               <button onClick={() => acceptRequest(request._id)} className="button">Accept</button>
