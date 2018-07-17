5b248e97ff287713d46720fe23af1a5fc47b38f2
Bianca Jemsten <biancajemsten@gmail.com>
2018-07-17 11:11:03 +0100

    fixed event show style bugs

--- src/components/events/PendingRequests.js
+++ src/components/events/PendingRequests.js
@@ -3,7 +3,7 @@ import React from 'react';
 const PendingRequests = ({ event, acceptRequest, declineRequest }) => {
   return (
     <div>
-      <h3 className="title is-3">Pending Requests</h3>
+      {event.joinRequests.length > 0 && <h3 className="title is-3">Pending Requests</h3>}
       {event.joinRequests.map(request =>
         <div key={request._id}>
           <p>{request.username}</p>
