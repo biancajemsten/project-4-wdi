4472dd2ccff599e07af0d3422aa39f19081f6971
Bianca Jemsten <biancajemsten@gmail.com>
2018-07-18 09:23:23 +0100

    Setting up for deployment

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const path = require('path');
 
 
 const { dbURI, port } = require('./config/environment');
-
-//Set static path
-app.use(express.static(path.join(__dirname, 'client')));
 
 
 const errorHandler = require('./lib/errorHandler');
@@ -18,12 +14,13 @@ const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
 mongoose.connect(dbURI);
-//
-// app.use(express.static(`${__dirname}/public`));
-// app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
+app.use(express.static(`${__dirname}/public`));
 
 app.use(bodyParser.json());
 app.use('/api', router);
+app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Express running on port ${port}`));
